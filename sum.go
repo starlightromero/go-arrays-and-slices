7eacf53f674fc9628f63d511df2cf0342e80f8d2
Starlight Romero <starlightromero@protonmail.com>
2021-04-14 12:58:30 -0700

    [update] complete all functions and tests

--- sum.go
+++ sum.go
@@ -1,8 +1,34 @@
 package main
 
+// Sum sums the ints in a given slice
 func Sum(numbers []int) (sum int) {
-    for _, number := range numbers {
-        sum += number
-    }
-    return
+	for _, number := range numbers {
+		sum += number
+	}
+	return
+}
+
+// SumAll sums the totals of each slice
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+// SumAllTails sums the totals of the tails of each slice
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
 }
